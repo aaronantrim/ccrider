6875facfdebae60637f2e7585ac38b405b77e501
Aaron Antrim <aaron@trilliumtransit.com>
2014-09-22 12:35:41 -0700

    functions.php - show template

--- functions.php
+++ functions.php
@@ -1,3 +1,12 @@
 <?php
 // Exit if accessed directly
 if ( !defined('ABSPATH')) exit;
+
+/* Add custom functions below */
+
+add_action('wp_head', 'show_template'); 
+
+function show_template() { 	
+global $template;
+print_r('<!--'.$template.'-->');
+}
